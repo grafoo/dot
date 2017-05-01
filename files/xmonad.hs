16d3f4448de4f16126258e5e7c5005d911420df1
Manuel Graf <m@grafoo.at>
2017-05-02 00:25:53 +0200

    xmonad

---
+++ files/xmonad.hs
@@ -0,0 +1,22 @@
+import XMonad
+import XMonad.Config.Desktop
+import XMonad.Util.EZConfig (additionalKeysP)
+import XMonad.Layout.NoBorders (noBorders)
+import XMonad.Layout.ResizableTile  -- ResizableTall
+import XMonad.Actions.WindowBringer (gotoMenu, bringMenu)
+import XMonad.Layout.Magnifier (maximizeVertical, MagnifyMsg(Toggle))
+
+
+main :: IO ()
+main = do
+  xmonad $ desktopConfig
+    { terminal = "st -f 'Iosevka-11'"
+    , focusedBorderColor = "#00FF00"
+    , modMask = mod4Mask        -- use windows key
+    , layoutHook = desktopLayoutModifiers $
+                   maximizeVertical
+                   (ResizableTall 1 (1.5/100) (3/5) []) ||| (noBorders Full)
+    } `additionalKeysP` [ ("M-i", sendMessage Toggle)
+                        , ("M-g", gotoMenu) -- open dmenu and goto selected window
+                        , ("M-b", bringMenu) -- open dmenu and bring selected window into current workspace
+                        ]
